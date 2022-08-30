23db5c7773174917c23a6cebc9bf75ff457b6b08
fuqiang <823342479@qq.com>
2022-08-30 15:42:27 +0800

    feat: add components-form

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
 import MiniRouter from "../components/Mini-router/index";
+import Form from "../components/Form";
 import DemoHook from "../hooks/demoHook";
 export const routes = [
   {
     path: "/components/mini-router",
     element: MiniRouter
+  },
+  {
+    path: "/components/form",
+    element: Form
   },
   {
     path: "/hooks/demo",
@@ -15,9 +20,13 @@ export const menus = [
   {
     title: "组件类",
     children: [
+      // {
+      //   path: "/components/mini-router",
+      //   label: "Mini-Router"
+      // },
       {
-        path: "/components/mini-router",
-        label: "Mini-Router"
+        path: "/components/form",
+        label: "Form"
       }
     ]
   },
